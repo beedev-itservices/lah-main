e589ade6068c2db64681f801ce1a3ce0295dd94c
Melissa Longenberger <242424+melissa-24@users.noreply.github.com>
2020-04-21 21:45:42 -0400

    fixing core.js

--- js/core.js
+++ js/core.js
@@ -1,18 +1,3 @@
-// Carousel
-var myIndex = 0;
-carousel();
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 5000); // Time in Milliseconds
-    };
-
 // Mobile Menu
 function myMenu() {
     var x = document.getElementById("myLinks");
@@ -34,3 +19,18 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     s0.parentNode.insertBefore(s1,s0);
     }
 )();
+
+// Carousel
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); // Time in Milliseconds
+    };
